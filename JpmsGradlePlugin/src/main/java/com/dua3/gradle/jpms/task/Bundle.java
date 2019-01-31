d82d2c55f1f0e4fb8225e78b4c3b424e3e674c5c
Axel Howind <axel@dua3.com>
2019-01-31 17:30:57 +0100

    bundle type 'installer' refers to default installer type for platorm

--- JpmsGradlePlugin/src/main/java/com/dua3/gradle/jpms/task/Bundle.java
+++ JpmsGradlePlugin/src/main/java/com/dua3/gradle/jpms/task/Bundle.java
@@ -3,11 +3,13 @@ package com.dua3.gradle.jpms.task;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 import com.dua3.gradle.jpms.JpmsGradlePlugin;
 import com.dua3.gradle.jpms.task.TaskHelper.ToolRunner;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskAction;
 
@@ -29,10 +31,10 @@ public class Bundle extends DefaultTask {
 		TaskHelper.removeFolder(output);
         
 		// jpackager arguments
-		String type = TaskHelper.orDefault(extension.getType(), "image");
+		String type = determineBundleType(TaskHelper.orDefault(extension.getType(), "image"));
 		String name = TaskHelper.orDefault(extension.getName(), project.getName());
 		String version = TaskHelper.orDefault(extension.getVersion(), "SNAPSHOT");
-		String mainJar = project.getName()+".jar";
+		String mainJar = TaskHelper.orDefault(extension.getMainJar(), project.getName()+".jar");
 		String appClass = extension.getAppClass();
 		String runtimeImage = TaskHelper.getOutputFolder(project, JLink.FOLDER_NAME);
 		String[] extraArgs = extension.getExtraArgs();
@@ -65,6 +67,28 @@ public class Bundle extends DefaultTask {
 		int rc = TaskHelper.runTool(tool, project, args);
 		if (rc!=0) {
 			throw new IllegalStateException("Executing "+tool+" failed with exit status "+ rc);
+		}
+	}
+
+	private String determineBundleType(String t) {
+		switch (t) {
+		case "":
+		case "image":
+			return "image";
+		case "installer":
+			String os = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+			if (os.contains("windows")) {
+				return "msi";
+			}
+			if (os.contains("mac")) {
+				return "pkg";
+			}
+			if (os.contains("linux")) {
+				return "deb";
+			}
+			throw new GradleException("Could not determine installer type for system. Set type explicitly.");
+		default:
+			return t;
 		}
 	}
 
