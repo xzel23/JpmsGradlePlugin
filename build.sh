7bdbd0408717686f5c6b490501cc8d0378313bbb
Axel Howind <axel@dua3.com>
2019-03-17 13:33:08 +0100

    disable gradle daemon, use env in shebang

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-(cd JpmsGradlePlugin && ./gradlew clean build publishToMavenLocal) \
-&& (cd TestJpmsGradlePlugin && ./gradlew clean build --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true && ./gradlew run) \
-&& (cd TestJpmsGradlePlugin && ./gradlew eclipse --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true)
+#!/usr/bin/env bash
+(cd JpmsGradlePlugin && ./gradlew --no-daemon clean build publishToMavenLocal) \
+&& (cd TestJpmsGradlePlugin && ./gradlew --no-daemon clean build --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true && ./gradlew run) \
+&& (cd TestJpmsGradlePlugin && ./gradlew --no-daemon eclipse --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true)
