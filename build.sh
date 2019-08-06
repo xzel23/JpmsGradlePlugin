c6846283c914318667ca019ac62ebd4110ca7385
Axel Howind <axel@dua3.com>
2019-08-06 13:29:40 +0200

    pass version on command line

--- build.sh
+++ build.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/env bash
-(cd JpmsGradlePlugin && ./gradlew --no-daemon clean build publishToMavenLocal) \
+
+if [ $# -gt 1 ] ; then
+    echo "ERROR"
+    echo "to build and test the plugin: $0"
+    echo "to release a new version of the plugin: $0 -Pversion=<version>"
+    exit 1
+fi
+
+if [ $# -ge 1 ] ; then
+    VERSION=$1
+else
+    VERSION="SNAPSHOT"
+fi
+
+echo 
+echo "building as version $VERSION"
+echo 
+
+VERSION_ARG="-Pversion=${VERSION}"
+EXTRA_ARGS="--no-daemon --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true"
+
+(cd JpmsGradlePlugin && ./gradlew ${VERSION_ARG} --no-daemon clean build publishToMavenLocal) \
 && echo "========== JDK9 ==========" \
 && (cd TestJpmsGradlePlugin \
-    && ./gradlew --no-daemon clean build --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true \
-    && ./gradlew --no-daemon test --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true \
-    && ./gradlew --no-daemon run --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true \
-    && ./gradlew --no-daemon eclipse --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true) \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} clean build \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} test \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} run \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} eclipse) \
 && echo "========== JDK8 ==========" \
 && (cd TestJpmsGradlePlugin-Jdk8 \
-    && ./gradlew --no-daemon clean build --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true \
-    && ./gradlew --no-daemon test --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true \
-    && ./gradlew --no-daemon run --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true \
-    && ./gradlew --no-daemon eclipse --stacktrace -DDEBUG_JPMS_GRADLE_PLUGIN=true) \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} clean build \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} test \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} run \
+    && ./gradlew ${VERSION_ARG} ${EXTRA_ARGS} eclipse) \
